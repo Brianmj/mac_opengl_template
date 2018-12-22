2570c45100dd90f0d52c6fc334b093e696957244
brianmj <spamthiseml@hotmail.com>
2018-12-21 21:35:43 -0500

    Initial Commit

---
+++ glfw_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  glfw_test
+//
+//  Created by Brian Jones on 12/21/18.
+//  Copyright © 2018 Brian Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
